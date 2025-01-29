1441b89bce61f3d0ee463f7ca693bc00469ce08b
Bernardo Cafiero Viana <cabewill@gmail.com>
2025-01-29 06:43:46 -0700

    Change import task

--- init_server.sh
+++ init_server.sh
@@ -26,7 +26,9 @@ if [ ! -d "$REPO_DIR" ]; then
     exit 1
   fi
 else
-  echo "Repository is already cloned."
+  echo "Repository is already cloned. Pulling last updates"
+  cd "$REPO_DIR"
+  git pull https://github.com/cabewill/final_project.git 
 fi
 
   # export ANSIBLE_HOST_KEY_CHECKING=False
@@ -41,7 +43,7 @@ fi
 
 # Run the webapp deployment playbook
 echo "Running webapp deployment playbook..."
-ansible-playbook -i localhost, $REPO_DIR/playbooks/webapp_deploy.yml
+ansible-playbook -i localhost -c local, $REPO_DIR/playbooks/webapp_deploy.yml
 if [ $? -ne 0 ]; then
   echo "Error: Webapp deployment playbook failed."
   exit 1
@@ -49,7 +51,7 @@ fi
 
 # Run the task deployment playbook
 echo "Running task deployment playbook..."
-ansible-playbook -i localhost, $REPO_DIR/playbooks/task_deploy.yml
+ansible-playbook -i localhost -c local, $REPO_DIR/playbooks/task_deploy.yml
 if [ $? -ne 0 ]; then
   echo "Error: Task deployment playbook failed."
   exit 1
