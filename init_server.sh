cecd424ca0aafb6c57a467a05330150fa77fa935
Bernardo Cafiero Viana <cabewill@gmail.com>
2025-01-29 15:15:03 -0700

    Add html page

--- init_server.sh
+++ init_server.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+  REPO_DIR="/tmp/final_project"
+
   
   # Check if Ansible is installed
   if ! command -v ansible-playbook &> /dev/null; then
@@ -16,20 +18,25 @@ set -euo pipefail
     echo "Ansible is already installed."
   fi
 
+  # cp -r ./playbooks $REPO_DIR/playbooks
+  # cp -r ./webapp $REPO_DIR/webapp
+
   # Check if the repository is already cloned
-REPO_DIR="/tmp/final_project"
-if [ ! -d "$REPO_DIR" ]; then
-  echo "Cloning repository..."
-  git clone https://github.com/cabewill/final_project.git "$REPO_DIR"
-  if [ $? -ne 0 ]; then
-    echo "Error: Git clone failed."
-    exit 1
+
+  if [ ! -d "$REPO_DIR" ]; then
+    echo "Cloning repository..."
+    git clone https://github.com/cabewill/final_project.git "$REPO_DIR"
+    if [ $? -ne 0 ]; then
+      echo "Error: Git clone failed."
+      exit 1
+    fi
+  else
+    echo "Repository is already cloned. Pulling last updates"
+    cd "$REPO_DIR"
+    git pull
   fi
-else
-  echo "Repository is already cloned. Pulling last updates"
-  cd "$REPO_DIR"
-  git pull
-fi
+
+
 
   # export ANSIBLE_HOST_KEY_CHECKING=False
 
